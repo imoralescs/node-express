b2b6452f0c7a9f4dc11433e98a992f08a3f90c20
Israel Morales <israel@savvycard.com>
2019-06-20 15:03:40 -0400

    message

--- server/index.js
+++ server/index.js
@@ -1,12 +1,36 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
 
 const app = express()
+const port = process.env.PORT || 8000
 
-app.all('*', (req, res) => {
-    res.json({ ok: true })
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (request, response) => {
+    response
+        .render('index', {
+            title: 'Home'
+        })
 })
 
-app.listen(3000, () => {
-    console.log(`Server stated at http://localhost:${3000}`)
+app.get('/user', (request, response) => {
+    response
+        .render('user', {
+            title: 'Profile',
+            userProfile: {
+                username: 'Auth0'
+            }
+        })
+})
+
+app.all('*', (request, response) => {
+    response
+        .status(200)
+        .send('Hi, Developers')
+})
+
+app.listen(port, () => {
+    console.log(`Server stated at http://localhost:${port}`)
 })
